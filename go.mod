7f502e6f6330bada473ec22727b968373672283c
panyanyany <568397440@qq.com>
2022-12-09 15:44:28 +0800

    add proxy

--- go.mod
+++ go.mod
@@ -4,5 +4,9 @@ go 1.16
 
 require (
 	github.com/eatmoreapple/openwechat v1.2.1
+	github.com/parnurzeal/gorequest v0.2.16 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	moul.io/http2curl v1.0.0 // indirect
 )
--- gtp/gtp.go
+++ gtp/gtp.go
@@ -1,44 +1,43 @@
 package gtp
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"github.com/869413421/wechatbot/config"
-	"io/ioutil"
-	"log"
-	"net/http"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "github.com/869413421/wechatbot/config"
+    "github.com/parnurzeal/gorequest"
+    "log"
+    "time"
 )
 
 const BASEURL = "https://api.openai.com/v1/"
 
 // ChatGPTResponseBody 请求体
 type ChatGPTResponseBody struct {
-	ID      string                 `json:"id"`
-	Object  string                 `json:"object"`
-	Created int                    `json:"created"`
-	Model   string                 `json:"model"`
-	Choices []ChoiceItem           `json:"choices"`
-	Usage   map[string]interface{} `json:"usage"`
+    ID      string                 `json:"id"`
+    Object  string                 `json:"object"`
+    Created int                    `json:"created"`
+    Model   string                 `json:"model"`
+    Choices []ChoiceItem           `json:"choices"`
+    Usage   map[string]interface{} `json:"usage"`
 }
 
 type ChoiceItem struct {
-	Text         string `json:"text"`
-	Index        int    `json:"index"`
-	Logprobs     int    `json:"logprobs"`
-	FinishReason string `json:"finish_reason"`
+    Text         string `json:"text"`
+    Index        int    `json:"index"`
+    Logprobs     int    `json:"logprobs"`
+    FinishReason string `json:"finish_reason"`
 }
 
 // ChatGPTRequestBody 响应体
 type ChatGPTRequestBody struct {
-	Model            string  `json:"model"`
-	Prompt           string  `json:"prompt"`
-	MaxTokens        int     `json:"max_tokens"`
-	Temperature      float32 `json:"temperature"`
-	TopP             int     `json:"top_p"`
-	FrequencyPenalty int     `json:"frequency_penalty"`
-	PresencePenalty  int     `json:"presence_penalty"`
+    Model            string  `json:"model"`
+    Prompt           string  `json:"prompt"`
+    MaxTokens        int     `json:"max_tokens"`
+    Temperature      float32 `json:"temperature"`
+    TopP             int     `json:"top_p"`
+    FrequencyPenalty int     `json:"frequency_penalty"`
+    PresencePenalty  int     `json:"presence_penalty"`
 }
 
 // Completions gtp文本模型回复
@@ -47,54 +46,54 @@ type ChatGPTRequestBody struct {
 //-H "Authorization: Bearer your chatGPT key"
 //-d '{"model": "text-davinci-003", "prompt": "give me good song", "temperature": 0, "max_tokens": 7}'
 func Completions(msg string) (string, error) {
-	requestBody := ChatGPTRequestBody{
-		Model:            "text-davinci-003",
-		Prompt:           msg,
-		MaxTokens:        1024,
-		Temperature:      0.7,
-		TopP:             1,
-		FrequencyPenalty: 0,
-		PresencePenalty:  0,
-	}
-	requestData, err := json.Marshal(requestBody)
+    requestBody := ChatGPTRequestBody{
+        Model:            "text-davinci-003",
+        Prompt:           msg,
+        MaxTokens:        1024,
+        Temperature:      0.7,
+        TopP:             1,
+        FrequencyPenalty: 0,
+        PresencePenalty:  0,
+    }
+    requestData, err := json.Marshal(requestBody)
 
-	if err != nil {
-		return "", err
-	}
-	log.Printf("request gtp json string : %v", string(requestData))
-	req, err := http.NewRequest("POST", BASEURL+"completions", bytes.NewBuffer(requestData))
-	if err != nil {
-		return "", err
-	}
+    if err != nil {
+        return "", err
+    }
+    log.Printf("request gtp json string : %v", string(requestData))
 
-	apiKey := config.LoadConfig().ApiKey
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+apiKey)
-	client := &http.Client{}
-	response, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer response.Body.Close()
-	if response.StatusCode != 200 {
-		return "", errors.New(fmt.Sprintf("gtp api status code not equals 200,code is %d", response.StatusCode))
-	}
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return "", err
-	}
+    apiKey := config.LoadConfig().ApiKey
 
-	gptResponseBody := &ChatGPTResponseBody{}
-	log.Println(string(body))
-	err = json.Unmarshal(body, gptResponseBody)
-	if err != nil {
-		return "", err
-	}
+    request := gorequest.New()
+    request.Post(BASEURL + "completions").
+        Timeout(60 * time.Second).
+        Proxy("http://127.0.0.1:7890").
+        Send(string(requestData))
 
-	var reply string
-	if len(gptResponseBody.Choices) > 0 {
-		reply = gptResponseBody.Choices[0].Text
-	}
-	log.Printf("gpt response text: %s \n", reply)
-	return reply, nil
+    request.AppendHeader("Content-Type", "application/json")
+    request.AppendHeader("Authorization", "Bearer "+apiKey)
+
+    resp, body, errs := request.
+        EndBytes()
+
+    if len(errs) > 0 {
+        return "", errors.New(fmt.Sprintf("gtp api has errors: %v", errs))
+    }
+    if resp.StatusCode != 200 {
+        return "", errors.New(fmt.Sprintf("gtp api status code not equals 200,code is %d, %v", resp.StatusCode, body))
+    }
+
+    gptResponseBody := &ChatGPTResponseBody{}
+    log.Println(string(body))
+    err = json.Unmarshal(body, gptResponseBody)
+    if err != nil {
+        return "", err
+    }
+
+    var reply string
+    if len(gptResponseBody.Choices) > 0 {
+        reply = gptResponseBody.Choices[0].Text
+    }
+    log.Printf("gpt response text: %s \n", reply)
+    return reply, nil
 }
